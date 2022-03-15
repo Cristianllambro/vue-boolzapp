19f5621a98ce90c22c6bb453c309c999d8fa5165
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-15 16:56:08 +0100

    working on message

---
+++ script.js
@@ -0,0 +1,40 @@
+const app = new Vue ({
+    el: '#root',
+    data: {
+        arrChats: 
+        [
+            {
+                name: 'Michele',
+                img: 'avatar_1.jpg'
+            },
+            {
+                name: 'Fabio',
+                img: 'avatar_2.jpg'
+            },
+            {
+                name: 'Samuele',
+                img: 'avatar_3.jpg'
+            },
+            {
+                name: 'Alessandro B.',
+                img: 'avatar_4.jpg'
+            },
+            {
+                name: 'Alessandro U.',
+                img: 'avatar_5.jpg'
+            },
+            {
+                name: 'Claudia',
+                img: 'avatar_6.jpg'
+            },
+            {
+                name: 'Federico',
+                img: 'avatar_7.jpg'
+            },
+            {
+                name: 'Davide',
+                img: 'avatar_8.jpg'
+            },
+        ]
+    }
+});
