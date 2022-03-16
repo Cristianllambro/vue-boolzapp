c1e4b38aef74b02fd9cd677da53715d6de04c93a
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-16 21:46:06 +0100

    workink on last message and time

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const app = new Vue ({
         currentChatIndex: 0,
 
         newMessage: {
-            mess: 'Ok',
             message: '',
+            sent: true,
         },
 
         arrChats:
@@ -13,115 +13,190 @@ const app = new Vue ({
             {
                 name: 'Michele',
                 img: 'avatar_1.jpg',
-                mess: 'Tutto fatto!',
+                date: luxon.DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss'),
+                time: luxon.DateTime.now().toFormat("HH:mm"),
                 text: 
                 [
                     {
                         message: 'Hai portato a spasso il cane?',
+                        sent: true,
+                        
                     },
                     {
                         message: 'Ricordati di stendere i panni',
+                        sent: true,
+                    },
+                    {
+                        message: 'Tutto fatto!',
+                        sent: false,
                     },
                 ],
             },
             {
                 name: 'Fabio',
                 img: 'avatar_2.jpg',
-                mess: 'Certo',
+                date: luxon.DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss'),
+                time: luxon.DateTime.now().toFormat("HH:mm"),
                 text:
                 [
                     {
                         message: 'Hai studiato CSS?',
+                        sent: true,   
+                    },
+                    {
+                        message: 'Certo',
+                        sent: false,
                     },
                 ],
             },
             {
                 name: 'Samuele',
-                img: 'avatar_3.jpg',
-                mess: 'Sempre forza Roma!',
+                img: 'avatar_3.jpg', 
+                date: luxon.DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss'),
+                time: luxon.DateTime.now().toFormat("HH:mm"),    
                 text:
                 [
                     {
                         message: 'Forza Roma!',
+                        sent: true,
                     },
                     {
                         message: 'Non tifare Lazio!',
+                        sent: true,
+                    },
+                    {
+                        message: 'Sempre forza Roma!',
+                        sent: false
                     },
                 ],
             },
             {
                 name: 'Alessandro B.',
                 img: 'avatar_4.jpg',
-                mess: 'Si!',
+                date: luxon.DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss'),
+                time: luxon.DateTime.now().toFormat("HH:mm"),
                 text:
                 [
                     {
                         message: 'Hai capito Bootstrap?',
+                        sent: true,
+                    },
+                    {
+                        message: 'Si!',
+                        sent: false,
                     },
                 ],
             },
             {
                 name: 'Alessandro U.',
                 img: 'avatar_5.jpg',
-                mess: 'Hahahahahahaha!',
+                date: luxon.DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss'),
+                time: luxon.DateTime.now().toFormat("HH:mm"),
                 text:
                 [
                     {
                         message: 'Come si fa quella cosa in JS?',
+                        sent: true,
                     },
                     {
                         message: 'Non ho capito nulla delle funzioni!',
+                        sent: true,
+                    },
+                    {
+                        message: 'Certo',
+                        sent: false,
                     },
                 ],
             },
             {
                 name: 'Claudia',
                 img: 'avatar_6.jpg',
-                mess: 'Va bene',
+                date: luxon.DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss'),
+                time: luxon.DateTime.now().toFormat("HH:mm"),
                 text:
                 [
                     {
                         message: 'Stasera usciamo?',
+                        sent: true,
                     },
                     {
                         message: 'Andiamo nel bar nuovo',
+                        sent: true,
+                    },
+                    {
+                        message: 'Va bene',
+                        sent: false,
                     },
                 ],
             },
             {
                 name: 'Federico',
                 img: 'avatar_7.jpg',
-                mess: 'Va bene',
+                date: luxon.DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss'),
+                time: luxon.DateTime.now().toFormat("HH:mm"),
                 text:
                 [
                     {
                         message: 'Vado a giocare a calcio',
+                        sent: true,
                     },
                     {
                         message: 'Vieni?',
+                        sent: true,
+                    },
+                    {
+                        message: 'Va bene',
+                        sent: false,
                     },
                 ],
             },
             {
                 name: 'Davide',
                 img: 'avatar_8.jpg',
-                mess: 'Va bene',
+                date: luxon.DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss'),
+                time: luxon.DateTime.now().toFormat("HH:mm"),
                 text:
                 [
                     {
                         message: 'Facciamo una partita a D&D?',
+                        sent: true,
                     },
                     {
                         message: 'Una bella campagna con il mago',
+                        sent: true,
+                    },
+                    {
+                        message: 'Va bene',
+                        sent: false,
                     },
                 ],
             },
         ],
     },
     methods: {
+
         addMessage() {
-            const newMessage = {...this.newMessage};
-            this.arrChats[this.currentChatIndex].text.message.push(newMessage)
+            let newMessage = {...this.newMessage};
+            this.newMessage.message = '';
+            this.postAddMessage(newMessage)
+            setTimeout(this.otherMess, 1000);
+        },
+
+        postAddMessage(newMessage) {
+            this.arrChats[this.currentChatIndex].text.push(newMessage);
+        },
+
+        otherMess(){
+            this.postAddMessage({
+                message: 'ok',
+                sent: false,
+            });
+        },
+
+        filterName() {
+
+            const NameFilter = this.arrChats.filter(arrChats => this.arrChats.name == this.name);
+            return NameFilter;
         }
-    }
-})
+    },
+});
